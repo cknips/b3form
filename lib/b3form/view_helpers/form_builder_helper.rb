0a7eb522a8beda865b19c351c680bb262210356a
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2013-09-27 13:12:23 +0200

    bootstrap <form>

--- lib/b3form/view_helpers/form_builder_helper.rb
+++ lib/b3form/view_helpers/form_builder_helper.rb
@@ -2,7 +2,9 @@ module B3Form
   module ViewHelpers
     module FormBuilderHelper
       def b3_form_for(object, options = {}, &block)
-        options[:builder] = B3Form::FormBuilder
+        options[:builder]     = B3Form::FormBuilder
+        options[:html]      ||= {}
+        options[:html][:role] = 'form'
         form_for(object, options, &block)
       end
     end
--- spec/views/basic_form_spec.rb
+++ spec/views/basic_form_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'basic form elements' do
   before { assign(:task, Task.new) }
 
-  it 'renders a form' do
+  it 'renders a form with role = "form" attribute' do
     render template: 'tasks/new.html.erb'
 
-    expect(page rendered).to have_css 'form'
+    expect(page rendered).to have_css 'form[role=form]'
   end
 end
