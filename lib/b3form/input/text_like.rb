b82bbbc619b0a87402289408deee9a5c912fd4cb
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2013-11-13 19:25:56 +0100

    append and prepend buttons to text-like fields

--- lib/b3form/input/text_like.rb
+++ lib/b3form/input/text_like.rb
@@ -2,8 +2,11 @@ module B3Form
   class Input::TextLike < Input::TopLevelElement
 
     def render_input_group(&block)
-      input_prepend = options[:prepend]
-      input_append  = options[:append]
+      input_prepend        = options[:prepend]
+      input_append         = options[:append]
+
+      input_prepend_button = options[:prepend_button]
+      input_append_button  = options[:append_button]
 
       if input_prepend || input_append
         content_tag :div, class: 'input-group' do
@@ -15,6 +18,20 @@ module B3Form
           block.call +
           if input_append
             content_tag :span, input_append, class: 'input-group-addon'
+          else
+            ''.html_safe
+          end
+        end
+      elsif input_prepend_button || input_append_button
+        content_tag :div, class: 'input-group' do
+          if input_prepend_button
+            content_tag :div, input_prepend_button, class: 'input-group-btn'
+          else
+            ''.html_safe
+          end +
+          block.call +
+          if input_append_button
+            content_tag :div, input_append_button, class: 'input-group-btn'
           else
             ''.html_safe
           end
@@ -31,14 +48,16 @@ module B3Form
       input_options = super
 
       add_to_options(input_options, :class, 'form-control')
+      
       input_options[:placeholder] = placeholder_text if placeholder_text
+      input_options[:value]       = options[:value]  if options[:value]
 
       input_options
     end
 
 
     def placeholder_text
-      option_or_i18n(:placeholder)
+      @placeholder_text ||= option_or_i18n(:placeholder)
     end
   end
 end
