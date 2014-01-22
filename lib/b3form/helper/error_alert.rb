00aa8bd21211454acd12e8eb10c3c46aa68ff08b
Tilo Dietrich <tilo.dietrich@knips-konsorten.de>
2014-01-22 14:19:40 +0100

    error messages in error alert can now contain HTML

--- lib/b3form/helper/error_alert.rb
+++ lib/b3form/helper/error_alert.rb
@@ -5,7 +5,7 @@ module B3Form
 
       if errors[field].any?
         content_tag :div, class: 'alert alert-danger' do
-          errors.full_messages_for(field).join(', ')
+          errors.full_messages_for(field).join(', ').html_safe
         end
       end
     end
